771676b5eef05edd54f1b360fc63404f6580ada5
Ilia Ostashkov <Ilia_Ostashkov@epam.com>
2021-09-15 09:50:59 +0300

    detail request called

--- lib/pages/art_object_detail/bloc/art_object_detail_bloc.dart
+++ lib/pages/art_object_detail/bloc/art_object_detail_bloc.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:art_object_repository/art_object_repository.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test_app/constants/fetch_error_constants.dart';
 import 'package:flutter_test_app/pages/art_object_detail/bloc/art_object_detail_event.dart';
 import 'package:flutter_test_app/pages/art_object_detail/bloc/art_object_detail_state.dart';
 
@@ -21,9 +26,43 @@ class ArtObjectDetailBloc
 
   Future<ArtObjectDetailState> _mapInitialEventToState(
       ArtObjectDetailInitialEvent event) async {
-    return state.copyWith(
-      status: ArtObjectDetailStatus.success,
-      artObject: event.artObject,
-    );
+    if (state.status != ArtObjectDetailStatus.initialLoading) {
+      return state;
+    }
+    try {
+      final ArtObject artObjectDetail = await repository.getArtObject(
+        objectNumber: event.artObject.objectNumber,
+      );
+      return state.copyWith(
+        status: ArtObjectDetailStatus.success,
+        artObject: artObjectDetail,
+      );
+    } on ArtObjectException {
+      return state.copyWith(
+        status: ArtObjectDetailStatus.failure,
+        errorMessage: FetchErrorConstants.serverError,
+      );
+    } on PlatformException catch (e) {
+      return state.copyWith(
+        status: ArtObjectDetailStatus.failure,
+        errorMessage: e.message,
+      );
+    } on SocketException catch (_) {
+      return state.copyWith(
+        status: ArtObjectDetailStatus.failure,
+        errorMessage: FetchErrorConstants.noInternetConnection,
+      );
+    } on TimeoutException {
+      return state.copyWith(
+        status: ArtObjectDetailStatus.failure,
+        errorMessage: FetchErrorConstants.timeout,
+      );
+    } catch (e) {
+      print(e);
+      return state.copyWith(
+        status: ArtObjectDetailStatus.failure,
+        errorMessage: FetchErrorConstants.undefinedError,
+      );
+    }
   }
 }
