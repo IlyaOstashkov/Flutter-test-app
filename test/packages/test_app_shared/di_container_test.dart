382a89166741107f60c09a6ab631815e7a1fccef
Ilya Ostashkov <ostashkovilya@yandex.ru>
2021-11-03 23:43:55 +0300

    DIContainer tests

---
+++ test/packages/test_app_shared/di_container_test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:test_app_shared/test_app_shared.dart';
+
+void main() {
+  group('DIContainer -', () {
+    late DIContainer diContainer;
+
+    setUp(() {
+      diContainer = DIContainer();
+    });
+
+    test('container is initialized', () {
+      expect(diContainer, isNotNull);
+    });
+
+    test('map and get methods work as expected', () {
+      diContainer.map<_DummyService>(() => _DummyServiceImpl());
+      final service = diContainer.get<_DummyService>();
+      expect(service, isNotNull);
+      expect(service, isA<_DummyServiceImpl>());
+    });
+  });
+}
+
+abstract class _DummyService {}
+
+class _DummyServiceImpl implements _DummyService {}
