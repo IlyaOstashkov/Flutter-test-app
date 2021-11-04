3c83d4ec4bf6aa56784b9f195e2bb08666fa7fcf
Ilya Ostashkov <ostashkovilya@yandex.ru>
2021-11-04 15:17:18 +0300

    check unused filed warning

--- packages/test_app_blocs/lib/src/bloc_di_container.dart
+++ packages/test_app_blocs/lib/src/bloc_di_container.dart
@@ -7,6 +7,8 @@ import 'package:test_app_shared/test_app_shared.dart';
 class BlocDIContainer extends DIContainer {
   static final instance = BlocDIContainer();
 
+  final _flag = false;
+
   void initialise() {
     _registerApiClient();
     _registerRepositories();
